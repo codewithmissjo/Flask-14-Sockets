165ed53e762df9320bf538d4050a3a7623a499ac
Miss Jo <7433673+codewithmissjo@users.noreply.github.com>
2025-05-09 17:14:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,23 @@ from os import environ
 from flask import Flask, render_template#, request, redirect, session
 from flask_socketio import SocketIO, emit
 
-appFlask = Flask(__name__)
-appFlask.config['SECRET_KEY'] = 'secret!'
-app = SocketIO(appFlask)
+socketio = SocketIO()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketop.init(app)
 
 @app.route('/')
 def index():
   return render_template('index.html')
 
-@app.on('connect', namespace='/test')
+@socketio.on('connect', namespace='/test')
 def test_connect():
   emit('my connection', {'data': 'Connected'})
 
-@app.on('my new message', namespace='/test')
+@socketio.on('my new message', namespace='/test')
 def test_message(message):
   emit('new message', {'data': message['data']})
 
 if __name__ == '__main__':
-  HOST = environ.get('SERVER_HOST', 'localhost')
-  PORT = int(environ.get('SERVER_PORT', '5000'))
-  app.run(app, host=HOST, port=PORT)
+  socketio.run(app)
